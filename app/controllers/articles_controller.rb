0a11e0882ecf5773be4c5749d20e13132c218b90
Nikola <woleet@outlook.com>
2021-07-11 16:18:39 +0200

    Added artical form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,21 @@
 class ArticlesController < ApplicationController
 
   def show
-      byebug
-      @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @all_articles = Article.all
+  end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
   end
 
 end
