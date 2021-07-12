971fb190f9281b05425b2f6e25d471d0feaf19b9
Nikola <woleet@outlook.com>
2021-07-12 06:42:07 +0200

    Added CRUD operations as links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def index
@@ -9,13 +10,41 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
 
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    if @article.save
+      flash[:notice] = "Article was created succesfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated succesfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+  @article = Article.find(params[:id])
   end
 
 end
