f679dee7361a8623a0cc2c971825a8948f726c4a
Nikola <woleet@outlook.com>
2021-07-29 08:22:20 +0200

    Add one-to-many association between users and articles from back end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_025702) do
+ActiveRecord::Schema.define(version: 2021_07_28_053623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_07_24_025702) do
     t.string "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
