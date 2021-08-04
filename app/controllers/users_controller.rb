2f90767e0806932be8121adec0398f5188f4f080
Nikola <woleet@outlook.com>
2021-08-04 15:10:28 +0200

    Added user restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def show
-    @user = User.find(params[:id])
     @all_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -13,11 +16,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account changes has been saved"
       redirect_to @user
@@ -29,6 +30,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome in Bloggy #{@user.username}! You have succesfully signed up."
       redirect_to articles_path
     else
@@ -36,9 +38,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles has been succesfully deleted"
+    redirect_to root_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit or delete your own profile"
+      redirect_to @user
+    end
+  end
 end
